8c54f8c3bb0348a6439b8c5e1e8286056dc0da68
Gbenga Fagbemim <gbengaobafemi1@gmail.com>
2023-07-31 08:37:25 +0100

    add: form validation update

--- src/Forms/Helpers/index.js
+++ src/Forms/Helpers/index.js
@@ -1,47 +1,51 @@
 export const getRegExp = (type) => {
-  let regex = null;
+  let regex = null
   switch (type) {
-    case "email":
-      regex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g;
-      break;
-    case "password":
-      regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-      break;
+    case 'email':
+      regex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g
+      break
+    case 'password':
+      regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+      break
     default:
-      break;
+      break
   }
-  return regex;
-};
+  return regex
+}
 
 export const getSentenceFromCamelCase = (message) => {
-  let pattern = /[A-Za-z]/g;
-  let messages = message.match(pattern);
-  let errorMessage = "";
+  let pattern = /[A-Za-z]/g
+  let messages = message.match(pattern)
+  let errorMessage = ''
   for (let i = 0; i < messages.length; i++) {
     errorMessage +=
       messages[i] === messages[i].toUpperCase()
-        ? " " + messages[i].toLowerCase()
-        : messages[i];
+        ? ' ' + messages[i].toLowerCase()
+        : messages[i]
   }
-  return errorMessage.trim();
-};
+  return errorMessage.trim()
+}
 
-export default function CheckFormValidation(errors, data) {
-  const finalErrors = {};
+function CheckFormValidation(errors, data) {
+  const finalErrors = {}
+
   Object.keys(data).forEach((key) => {
+    // Check if the field is required (by checking if the 'required' property is true)
     if (
-      data[key] === "" ||
-      data[key] === {} ||
-      data[key] === undefined ||
-      data[key] === null
+      data[key]?.required &&
+      (data[key].value === '' ||
+        data[key].value === undefined ||
+        data[key].value === null)
     ) {
-      finalErrors[key] = `Please enter ${getSentenceFromCamelCase(key)}.`;
+      finalErrors[key] = `Please enter ${getSentenceFromCamelCase(key)}.`
     }
-  });
+  })
+
   Object.keys(errors).forEach((key) => {
-    if (errors[key] !== "") {
-      finalErrors[key] = errors[key];
+    if (errors[key] !== '') {
+      finalErrors[key] = errors[key]
     }
-  });
-  return finalErrors;
+  })
+
+  return finalErrors
 }
